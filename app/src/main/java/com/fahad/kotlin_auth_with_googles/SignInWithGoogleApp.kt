ae8cbc3a894c57d090d6f0712355f33c29a7bb44
fahad <fah8ad@outlook.com>
2024-01-12 11:24:29 +0300

    updat the file

--- app/src/main/java/com/fahad/kotlin_auth_with_googles/SignInWithGoogleApp.kt
+++
@@ -1,8 +0,0 @@
-package com.fahad.kotlin_auth_with_googles
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class SignInWithGoogleApp : Application(){
-}
